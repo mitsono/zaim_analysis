c80504a321b49eadd43a0bb1909b9e6c71aa3e22
mitsono <mitsono@abeam.com>
2020-05-12 18:09:08 +0900

    メッセージ出力順変更

--- mypack/zaim_to_line.py
+++ mypack/zaim_to_line.py
@@ -18,11 +18,18 @@ def main():
 
     # 先月の予実分析をLineに通知
     zic = zaim_info_creater.ZaimInfoCreater(last_month_start_date, today_date)
-    linepush.pushMessage(zic.get_balance())
-    linepush.pushMessage(zic.get_lastmonth_userate())
-    linepush.pushMessage(zic.get_currentmonth_userate())
-    linepush.pushMessage(zic.get_diff_userate())
-    linepush.pushMessage(zic.get_summary())
+
+    balance_str = zic.get_balance()
+    lastmonth_userate_str = zic.get_lastmonth_userate()
+    currentmonth_userate_str = zic.get_currentmonth_userate()
+    diff_userate_str = zic.get_diff_userate()
+    summary_str = zic.get_summary()
+
+    linepush.pushMessage(diff_userate_str)
+    linepush.pushMessage(summary_str)
+    linepush.pushMessage(balance_str)
+    linepush.pushMessage(lastmonth_userate_str)
+    linepush.pushMessage(currentmonth_userate_str)
 
     # 終了処理
     zic.end()
